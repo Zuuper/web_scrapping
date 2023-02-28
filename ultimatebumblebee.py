3131c5564256dde92c9f483d7e1181cbfeb40902
Aditya Ersa <40928069+Zuuper@users.noreply.github.com>
2023-02-28 17:33:22 +0800

    Update ultimatebumblebee.py

--- ultimatebumblebee.py
+++ ultimatebumblebee.py
@@ -1,6 +1,7 @@
 from utilities.scraper_bot.collect_surface_deep_data_and_image import collect_all_data
-from utilities.scraper_bot.scraper import scraper
+from utilities.scraper_bot.scraper import scraper, setup_keyword_for_surface_search, parent_directory
 
 if __name__ == "__main__":
-    scraper()
-    
+    data = setup_keyword_for_surface_search()
+    keyword_list = f'{parent_directory}/keywords/{data}'
+    scraper(keyword_list)
