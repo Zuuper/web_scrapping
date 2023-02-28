88c7aeeb0d568d2c0d37e011a32a80c85b88dee4
Aditya Ersa <40928069+Zuuper@users.noreply.github.com>
2023-02-28 17:29:29 +0800

    Update ultimatebumblebee.py

--- ultimatebumblebee.py
+++ ultimatebumblebee.py
@@ -1,4 +1,6 @@
 from utilities.scraper_bot.collect_surface_deep_data_and_image import collect_all_data
+from utilities.scraper_bot.scraper import scraper
 
 if __name__ == "__main__":
-    collect_all_data()
+    scraper()
+    
